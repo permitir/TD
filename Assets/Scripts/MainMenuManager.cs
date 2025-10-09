2e10d094a0585123002549f4d8fad6579cee2aef
permitir <cotxmc@gmail.com>
2025-10-09 22:11:27 +0100

    Added main menu button('m')

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    private bool isPaused = false;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            MainMenu();
+        }
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f; //Make sure timer is reset 
+        LevelManager.main.LoadMainMenu();
+    }   
+}
