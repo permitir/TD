12825fc81506b26723b7bbc9543ec1a0dc95da89
permitir <cotxmc@gmail.com>
2025-11-12 20:53:43 +0000

    User settings saving has been added, and fixed plot hover

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -10,15 +10,18 @@ public class SettingsManager : MonoBehaviour
     public void ChangeMasterVolume()
     {
         mainAudioMixer.SetFloat("Master", masterVol.value);
+        PlayerPrefs.SetFloat("Master", masterVol.value);
     }
 
     public void ChangeMusicVolume()
     {
         mainAudioMixer.SetFloat("Music", musicVol.value);
+        PlayerPrefs.SetFloat("Music", musicVol.value);
     }
 
     public void ChangeSFXVolume()
     {
         mainAudioMixer.SetFloat("SFX", sfxVol.value);
+        PlayerPrefs.SetFloat("SFX", sfxVol.value);
     }
 }
